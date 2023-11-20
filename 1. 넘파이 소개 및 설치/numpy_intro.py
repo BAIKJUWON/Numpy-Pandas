febb60e0f857235f5098208139e3d5317d7cda9d
CE_BAIKJUWON <75535083+BAIKJUWON@users.noreply.github.com>
2023-11-21 02:09:20 +0900

    Add files via upload

---
+++ 1. 넘파이 소개 및 설치/numpy_intro.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# 1차원 배열 생성
+arr1 = np.array([1, 2, 3])
+
+# 2차원 배열 생성
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+# 배열 연산
+result = arr1 + arr2
+
+print("1차원 배열:", arr1)
+print("2차원 배열:\n", arr2)
+print("배열 연산 결과:\n", result)
